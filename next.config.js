a4ecd871dd68cbe143dfdd13a887b683b6fea76c
dvaJi <francisco_p@outlook.com>
2021-05-18 20:16:50 -0400

    fix: add cache to workflow and add cdn to serve files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
+  assetPrefix: isProd
+    ? "https://cdn.statically.io/gh/dvaJi/dvaJi.github.io/gh-pages/"
+    : "",
   future: {
     webpack5: true,
   },
