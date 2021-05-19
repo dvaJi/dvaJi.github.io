0d6b1b542933d709e77b29124ac83788c714c1b4
dvaJi <francisco_p@outlook.com>
2021-05-19 11:04:23 -0400

    fix: generate .nojekyll to disable Jekyll processing

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
-  assetPrefix: isProd
-    ? "https://cdn.statically.io/gh/dvaJi/dvaJi.github.io/gh-pages/"
-    : "",
   future: {
     webpack5: true,
   },
